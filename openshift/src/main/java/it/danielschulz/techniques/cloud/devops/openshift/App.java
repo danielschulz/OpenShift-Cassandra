5459ab96ca4b459a533ec8ad3e993f53bb9192f3
Daniel Schulz <daniel.schulz@capgemini.com>
2016-11-20 20:44:38 +0100

    initial commit w/ twi maven submodules

---
+++ openshift/src/main/java/it/danielschulz/techniques/cloud/devops/openshift/App.java
@@ -0,0 +1,13 @@
+package it.danielschulz.techniques.cloud.devops.openshift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
